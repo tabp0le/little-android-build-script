6651e04272b16088c02addb727959d00036322a3
Tab Fitts <tfitts1212@gmail.com>
2017-03-16 16:42:36 -0700

    Works well now

--- syncbuild.sh
+++ syncbuild.sh
@@ -1,1 +1,41 @@
-repo sync && . build/envsetup.sh && source config.conf && breakfast $DEVICECODENAME && make installclean && brunch $DEVICECODENAME && cd $OUT && export FILENAME=$(ls |grep -m 1 RR*.zip) && export MD5SUMNAME=$(ls |grep -m 1 RR*.md5sum) && sh build/upload-sftp $FTPUSER@$FTPSERVER:$FTPPATH $FILENAME $MD5SUMNAME -oIdentityFile=$FTPSSHKEY && cd $OTAPATH && sed -i -- 's/RR.\+zip/'"$FILENAME"'/g' $OTAXML && git commit $OTAXML -S -m "'$OTACOMMITMSG'" && git push $OTAREPO
+#!/bin/bash
+
+repo sync
+
+source config.conf
+export ANDROID_BUILD_DIR=$(pwd)
+
+. build/envsetup.sh && breakfast $DEVICECODENAME && make installclean && brunch $DEVICECODENAME
+
+echo " "
+echo "Build completed. Preparing to upload."
+echo " "
+
+cd $OUT
+
+export FILENAME=$(ls |grep -m 1 RR*.zip)
+export MD5SUMNAME=$(ls |grep -m 1 RR*.md5sum)
+
+cd $ANDROID_BUILD_DIR
+
+echo " "
+echo "Uploading..."
+
+sh upload-sftp $FTPUSER@$FTPSERVER:$FTPPATH $OUT/$FILENAME $OUT/$MD5SUMNAME
+
+echo " "
+echo "Upload complete...Updating ota.xml."
+echo " "
+
+cd $OTAPATH && sed -i -- 's/RR.\+zip/'"$FILENAME"'/g' $OTAXML
+
+echo " "
+echo "Commiting and uploading ota.xml changes to Github"
+
+git commit $OTAXML -S -m "'$OTACOMMITMSG'"
+git push $OTAREPO
+
+cd $ANDROID_BUILD_DIR
+echo " "
+echo " "
+echo "Device build and upload complete!"
